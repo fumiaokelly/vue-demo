645379dc7a5edea0ff7e750f2cacc0188855da57
fumiaokelly <fumiaokelly@163.com>
2018-05-12 19:07:57 +0800

    a simple demo

---
+++ src/components/admin/admin.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="admin">
+    <h1>Admin Main Page</h1>
+    <h1>welcome {{userId}}</h1>
+    <a href="#" @click="quit">注销登录</a>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import {getCookie, delCookie} from '@/common/js/cookie.js'
+export default {
+  name: 'admin',
+  data () {
+    return {
+      userId: ''
+    }
+  },
+  created () {
+    let uname = getCookie('AdminId')
+    console.log('uname', uname)
+    this.userId = uname
+    if (uname === '') {
+      this.$router.push('/')
+    }
+  },
+  methods: {
+    quit () {
+      delCookie('AdminId')
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" ref="stylesheet/stylus">
+</style>
